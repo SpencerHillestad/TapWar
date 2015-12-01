1ed5f3590b10964de4bf4a762404430b041918ff
Spencer <student@shillestad.local>
2015-12-01 09:57:12 -0600

    Initial Commit

---
+++ TapWar(final)Tests/TapWar_final_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TapWar_final_Tests.swift
+//  TapWar(final)Tests
+//
+//  Created by shillestad on 12/1/15.
+//  Copyright © 2015 shillestad. All rights reserved.
+//
+
+import XCTest
+@testable import TapWar_final_
+
+class TapWar_final_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
